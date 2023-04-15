bf1edf5a13babea0b4c3aed518a20e38cc439c95
Victoria Ebong <79677353+victoriaEssien@users.noreply.github.com>
2023-04-15 20:56:13 +0100

    redesigned front page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
 // App.jsx - frontend
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { FiSun, FiMoon } from "react-icons/fi";
 import "./App.css";
 
+import Header from "./Components/Header";
+
 const App = () => {
   const [url, setUrl] = useState("");
   const [status, setStatus] = useState({});
+  const [formError, setFormError] = useState(null);
   const [theme, setTheme] = useState("light");
 
   // const apiUrl = process.env.REACT_APP_API_URL; // get the API URL from the environment variable
 
+  useEffect(() => {
+    let timer
+    if(formError) {
+      timer = setTimeout(() => {
+        setFormError(null)
+      }, 3000)
+    }
+    return() => {
+      clearTimeout(timer)
+    }
+  }, [formError])
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (url === '') {
+      setFormError('Enter a valid url')
+    }
     try {
       const response = await axios.post(import.meta.env.VITE_API_URL, { url });
       setStatus(response.data);
@@ -29,21 +47,19 @@ const App = () => {
 
   return (
     <div className={`App ${theme}`}>
-      <div className="toggle-theme" onClick={handleToggleTheme}>
+      {/* <div className="toggle-theme" onClick={handleToggleTheme}>
         {theme === "light" ? <FiMoon /> : <FiSun />}
-      </div>
-      <h1>LogIQ</h1>
+      </div> */}
+      <Header />
       <form onSubmit={handleSubmit}>
-        <label htmlFor="url">Enter URL:</label>
-        <input
-          type="text"
-          id="url"
-          value={url}
-          onChange={(e) => setUrl(e.target.value)}
-        />
+        <input type="text" id="url" 
+        value={url} 
+        onChange={(e) => setUrl(e.target.value)}
+        placeholder="Enter a web page URL"/>
         <button type="submit">Analyze</button>
       </form>
-      {status.isUp !== undefined && (
+      {formError && <p className="error">{formError}</p>}
+      {url !== '' && status.isUp !== undefined && (
         <div className={`status ${status.isUp ? "up" : "down"}`}>
           <p>Status: {status.isUp ? "UP" : "DOWN"}</p>
           {status.ipAddress && <p>IP Address: {status.ipAddress}</p>}
---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+function Header() {
+  return (
+    <header className="header">
+        <h1 className="logo">LogIQ</h1>
+    </header>
+  )
+}
+
+export default Header
