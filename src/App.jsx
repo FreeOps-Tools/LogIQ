ebb12bf3b35a8d5de12185a2cdd70e8b7f42bdb9
Victoria Ebong <79677353+victoriaEssien@users.noreply.github.com>
2023-04-22 12:14:21 +0100

    saved theme mode to local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-// App.jsx - frontend
-
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { FiSun, FiMoon } from "react-icons/fi";
@@ -11,26 +9,31 @@ const App = () => {
   const [url, setUrl] = useState("");
   const [status, setStatus] = useState({});
   const [formError, setFormError] = useState(null);
-  const [theme, setTheme] = useState("light");
-
-  // const apiUrl = process.env.REACT_APP_API_URL; // get the API URL from the environment variable
+  const [theme, setTheme] = useState(() => {
+    const savedTheme = localStorage.getItem("theme");
+    return savedTheme ? savedTheme : "light";
+  });
 
   useEffect(() => {
-    let timer
-    if(formError) {
+    let timer;
+    if (formError) {
       timer = setTimeout(() => {
-        setFormError(null)
-      }, 3000)
+        setFormError(null);
+      }, 3000);
     }
-    return() => {
-      clearTimeout(timer)
-    }
-  }, [formError])
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [formError]);
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+  }, [theme]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (url === '') {
-      setFormError('Enter a valid url')
+    if (url === "") {
+      setFormError("Enter a valid url");
     }
     try {
       const response = await axios.post(import.meta.env.VITE_API_URL, { url });
@@ -47,19 +50,22 @@ const App = () => {
 
   return (
     <div className={`App ${theme}`}>
-      {/* <div className="toggle-theme" onClick={handleToggleTheme}>
+      <div className="toggle-theme" onClick={handleToggleTheme}>
         {theme === "light" ? <FiMoon /> : <FiSun />}
-      </div> */}
-      <Header />
+      </div>
+      <Header theme={theme}/>
       <form onSubmit={handleSubmit}>
-        <input type="text" id="url" 
-        value={url} 
-        onChange={(e) => setUrl(e.target.value)}
-        placeholder="Enter a web page URL"/>
-        <button type="submit">Analyze</button>
+        <input
+          type="text"
+          id="url"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+          placeholder="Enter a web page URL"
+        />
+        <button type="submit" className={`button ${theme}`}>Analyze</button>
       </form>
       {formError && <p className="error">{formError}</p>}
-      {url !== '' && status.isUp !== undefined && (
+      {url !== "" && status.isUp !== undefined && (
         <div className={`status ${status.isUp ? "up" : "down"}`}>
           <p>Status: {status.isUp ? "UP" : "DOWN"}</p>
           {status.ipAddress && <p>IP Address: {status.ipAddress}</p>}
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,17 @@
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCodeFork, faStar } from '@fortawesome/free-solid-svg-icons'
 
-function Header() {
+function Header({ theme }) {
   return (
     <header className="header">
-        <h1 className="logo">LogIQ</h1>
+      <h1 className={`logo ${theme}`}>LogIQ</h1>
+      <nav>
+        <ul>
+          <li><FontAwesomeIcon icon={faStar} style={{color: '#DAAA3F', marginRight: '5px'}}/>5</li>
+          <li><FontAwesomeIcon icon={faCodeFork} style={{color: '#768390', marginRight: '5px'}}/>5</li>
+        </ul>
+      </nav>
     </header>
   )
 }
