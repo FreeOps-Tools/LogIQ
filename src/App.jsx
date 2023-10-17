b4656af8a2825af9836c47834546c3c5e6ff0e16
Sudhanshu Makwana <shudhanshumakwana@gmail.com>
2023-10-18 01:23:04 +0530

    Updated logo
    Added multiple requests feature

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { FiSun, FiMoon } from "react-icons/fi";
 import "./App.css";
-
-
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 
 const App = () => {
   const [url, setUrl] = useState("");
-  const [status, setStatus] = useState({});
+  const [status, setStatus] = useState([]);
   const [formError, setFormError] = useState(null);
   const [theme, setTheme] = useState(() => {
     const savedTheme = localStorage.getItem("theme");
@@ -39,10 +37,12 @@ const App = () => {
     }
     try {
       const response = await axios.post(import.meta.env.VITE_API_URL, { url });
-      setStatus(response.data);
+      setStatus((prevStatus) => [response.data, ...prevStatus]);
     } catch (error) {
-      console.error(error);
-      setStatus({ isUp: false, ipAddress: null, uptime: 0, responseTime: 0 });
+      setStatus((prevStatus) => [
+        { isUp: false, ipAddress: null, uptime: 0, responseTime: 0 },
+        ...prevStatus,
+      ]);
     }
   };
 
@@ -55,27 +55,36 @@ const App = () => {
       <div className="toggle-theme" onClick={handleToggleTheme}>
         {theme === "light" ? <FiMoon /> : <FiSun />}
       </div>
-      <Header theme={theme}/>
+      <Header theme={theme} />
       <form onSubmit={handleSubmit}>
         <input
           type="text"
           id="url"
           value={url}
           onChange={(e) => setUrl(e.target.value)}
-          placeholder="Enter a web page URL"
+          placeholder="http://example.com"
         />
-        <button type="submit" className={`button ${theme}`}>Analyze</button>
+        <button type="submit" className={`button ${theme}`}>
+          Analyze
+        </button>
       </form>
       {formError && <p className="error">{formError}</p>}
-      {url !== "" && status.isUp !== undefined && (
-        <div className={`status ${status.isUp ? "up" : "down"}`}>
-          <p>Status: {status.isUp ? "UP" : "DOWN"}</p>
-          {status.ipAddress && <p>IP Address: {status.ipAddress}</p>}
-          <p>Uptime: {status.uptime}%</p>
-          <p>Response Time: {status.responseTime}s</p>
+      {
+        <div>
+          {status.map((statusObj, idx) => (
+            <div
+              key={idx}
+              className={`status ${statusObj.isUp ? "up" : "down"}`}
+            >
+              <p>Status: {statusObj.isUp ? "UP" : "DOWN"}</p>
+              {statusObj.ipAddress && <p>IP Address: {statusObj.ipAddress}</p>}
+              {<p>Uptime: {statusObj.uptime}%</p>}
+              {<p>Response Time: {statusObj.responseTime}s</p>}
+            </div>
+          ))}
         </div>
-      )}
-      <Footer/>
+      }
+      <Footer />
     </div>
   );
 };
